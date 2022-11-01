7b59af1e049701df249d69a9021823a8250ebd16
mister-k9 <nynkay9@gmail.com>
2022-11-01 12:10:06 +0530

    corrected mispelled procfile

--- main/urls.py
+++ main/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('epani/', include('epani.urls')),
     path('accounts/', include('accounts.urls')),
 
-    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    ## re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]
